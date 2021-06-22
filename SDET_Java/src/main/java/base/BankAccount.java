96198da3da53373772fc542713a87553927d0cdb
Gustavo Cunha <occunha.g@gmail.com>
2021-06-22 20:27:13 -0300

    Add files via upload

---
+++ SDET_Java/src/main/java/base/BankAccount.java
@@ -0,0 +1,16 @@
+package base;
+
+public class BankAccount {
+    private String account;
+    public BankAccount(String account){
+        this.account = account;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+}
---
+++ SDET_Java/src/main/java/cucumber/steps/verifyBankAccountSteps.java
@@ -0,0 +1,11 @@
+package cucumber.steps;
+import io.cucumber.java.en.*;
+
+public class verifyBankAccountSteps {
+
+    @Given("^the bank account validation request with a valid (.*) and valid authentication <token>$")
+    public void givenTheConnection(String account){
+
+    }
+
+}
